fe5031ff05fcfb2b894a8ce8aaa1f57ce1f0b80f
bethbenjamin <bethbenjamin@gmail.com>
2018-03-23 14:10:52 -0400

    readme file, added to app: hide function, redirect function and mouseover/mouseout

--- README.txt
+++ README.txt
@@ -1,5 +1,11 @@
 I implemented javascript on this page using the following: 
 1.INDEX HOME:  body onload="Greeting()"  It greets the user and allows them to input their name.
-2.INDEX HOME:  id="home" onmouseover="mouseOver()" onmouseout="mouseOut().  When user mouses over the Welcome message, it changes to light blue and then back to white.
-3.
+
+2.INDEX HOME:  id="home" onmouseover="mouseOver()" onmouseout="mouseOut().  When user mouses over the Welcome message, 
+it changes to light blue and then back to white.
+
+3.Layout page: function redirect - this function allows users to leave the page to 'shop now'.  
+
+4. Tops page: Hide function - i have added an animation that can be 'hidden' with the close button.
+
 
--- WardrobeApplication/Content/JavaScript.js
+++ WardrobeApplication/Content/JavaScript.js
@@ -1,9 +1,21 @@
 ﻿
+//hide function
+function hide() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
 //welcome function
 function Greeting() {
     var x = document.getElementById("jumbomain");
     var userResponse = prompt('Hello,I am your Wardrobe Application. What should I call you?');
-    x.getElementsByTagName('h1')[0].innerText = userResponse +' - Welcome to your Wardrobe' ;
+    x.getElementsByTagName('h1')[0].innerText = userResponse + ' - Welcome to your Wardrobe';
 }
 
 
@@ -18,3 +30,13 @@ function mouseOver() {
 function mouseOut() {
     document.getElementById("home").style.color = "white";
 }
+
+
+// redirecting function
+function Redirect() {
+    window.open("https://www.tbdress.com/Cheap-Mens-Outerwears-105416/")
+}
+//function Redirect() {
+//    window.location = "http://www.tbdress.com", '_blank';
+
+//}
